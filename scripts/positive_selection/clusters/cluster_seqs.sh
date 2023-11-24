639bd213e7ace8581e1f3ebe904ed68ccc379896
Emma Hobbs <57451458+HobnobMancer@users.noreply.github.com>
2023-11-24 13:22:56 +0000

    Create cluster_seqs.sh
    Add script to cluster sequences for each CAZyme class

---
+++ scripts/positive_selection/clusters/cluster_seqs.sh
@@ -0,0 +1,15 @@
+pyrewton cluster_cazymes data/positive_selection/class_seqs/ce_seqs.fasta \
+  --mmseqs_db data/positive_selection/ce_clusters/mmseqs/mmseq_db \
+  --mmseqs_out data/positive_selection/ce_clusters/mmseqs/mmseq_out \
+  --out_tsv data/positive_selection/ce_clusters/ce_clusters.csv 
+  
+pyrewton cluster_cazymes data/positive_selection/class_seqs/pl_seqs.fasta \
+  --mmseqs_db data/positive_selection/pl_clusters/mmseqs/mmseq_db \
+  --mmseqs_out data/positive_selection/pl_clusters/mmseqs/mmseq_out \
+  --out_tsv data/positive_selection/pl_clusters/pl_clusters.csv 
+
+  pyrewton cluster_cazymes data/positive_selection/class_seqs/gh_seqs.fasta \
+  --mmseqs_db data/positive_selection/gh_clusters/mmseqs/mmseq_db \
+  --mmseqs_out data/positive_selection/gh_clusters/mmseqs/mmseq_out \
+  --out_tsv data/positive_selection/gh_clusters/gh_clusters.csv 
+  
