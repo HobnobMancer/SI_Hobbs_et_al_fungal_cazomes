6956b854f770a1c43a14a41bfa91b48f2784a3e1
Emma Hobbs <57451458+HobnobMancer@users.noreply.github.com>
2023-11-24 13:03:42 +0000

    Create get_seqs.sh
    Add script to build plain text files of class protein ids and extract the corresponding protein sequences from the local CAZome database

---
+++ scripts/positive_selection/get_seqs.sh
@@ -0,0 +1,35 @@
+DB="data/cazy/all_cazy_2020_01_13.db"
+
+# generate lists of protein IDs
+
+sqlite3 $DB -header -csv "
+SELECT DISTINCT Proteins.genbank_accession, CazyFamilies.family, Classifiers.classifier
+FROM Proteins
+INNER JOIN Domains on Proteins.protein_id = Domains.protein_id
+INNER JOIN CazyFamilies on Domains.family_id = CazyFamilies.family_id
+INNER JOIN Classifiers on Domains.classifier_id = Classifiers.classifier_id
+WHERE CazyFamilies.family like 'CE%' and Classifiers.classifier = 'dbCAN'
+" > data/positive_selection/class_prot_ids/ce_ids
+
+sqlite3 $DB -header -csv "
+SELECT DISTINCT Proteins.genbank_accession, CazyFamilies.family, Classifiers.classifier
+FROM Proteins
+INNER JOIN Domains on Proteins.protein_id = Domains.protein_id
+INNER JOIN CazyFamilies on Domains.family_id = CazyFamilies.family_id
+INNER JOIN Classifiers on Domains.classifier_id = Classifiers.classifier_id
+WHERE CazyFamilies.family like 'PL%' and Classifiers.classifier = 'dbCAN'
+" > data/positive_selection/class_prot_ids/pl_ids
+
+sqlite3 $DB -header -csv "
+SELECT DISTINCT Proteins.genbank_accession, CazyFamilies.family, Classifiers.classifier
+FROM Proteins
+INNER JOIN Domains on Proteins.protein_id = Domains.protein_id
+INNER JOIN CazyFamilies on Domains.family_id = CazyFamilies.family_id
+INNER JOIN Classifiers on Domains.classifier_id = Classifiers.classifier_id
+WHERE CazyFamilies.family like 'GH%' and Classifiers.classifier = 'dbCAN'
+" > data/positive_selection/class_prot_ids/gh_ids
+
+# Extract protein sequences
+pyrewton extract_db_seqs $DB data/positive_selection/class_prot_ids/ce_ids -o data/positive_selection/class_seqs/ce_seqs.fasta
+pyrewton extract_db_seqs $DB data/positive_selection/class_prot_ids/pl_ids -o data/positive_selection/class_seqs/pl_seqs.fasta
+pyrewton extract_db_seqs $DB data/positive_selection/class_prot_ids/gh_ids -o data/positive_selection/class_seqs/gh_seqs.fasta
